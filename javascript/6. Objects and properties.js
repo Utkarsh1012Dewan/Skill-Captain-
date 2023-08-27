19d517249ba885fff0b81e00c88bea2453e2dafa
Utkarsh1012Dewan <108482043+Utkarsh1012Dewan@users.noreply.github.com>
2023-08-27 13:29:42 +0530

    Update 6. Objects and properties.js

---
+++ javascript/6. Objects and properties.js
@@ -0,0 +1,19 @@
+let Book ={
+    title:'Crime and Punishment',
+    author:'Fyodor Dostoevesky',
+    year: 1866,
+    isAvailable: 'Yes'
+}
+
+console.log(Book.title)
+console.log(Book.author)
+console.log(Book.year)
+console.log(Book.isAvailable)
+
+
+console.log(Book['title'])
+console.log(Book['author'])
+console.log(Book['year'])
+console.log(Book['isAvailable'])
+
+
